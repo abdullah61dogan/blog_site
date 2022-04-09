d1147f1b6f1612e2aabe31f64955a2372a0cb1b1
Abdullah Doğan <abdullah.dogan@codeck.com.tr>
2022-04-09 06:25:42 +0300

    couldn't define the problem

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,25 @@
+from logging import lastResort
 from django.shortcuts import render
+from tomlkit import date
 
 # Create your views here.
 
+
+def get_date(post):
+    return post['date']
+
+
 def starting_page(request):
-  return render(request, "blog/index.html")
+    sorted_posts = posts.sort(key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+      "posts": latest_posts
+    })
+
 
 def posts(request):
-  return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html")
+
 
 def post_detail(request, slug):
-  return render(request, "blog/post-detail.html")
+    return render(request, "blog/post-detail.html")
